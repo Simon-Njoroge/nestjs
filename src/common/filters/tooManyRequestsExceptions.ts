2992819ebfb0bbd033fb87a05f46eedaf131ad06
Simon-Njoroge <mukirisimon22@gmail.com>
2025-06-14 22:50:45 +0300

    authorization enhancement

--- src/common/filters/tooManyRequestsExceptions.ts
+++ src/common/filters/tooManyRequestsExceptions.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
-// ...existing code...
+
 
 export class TooManyRequestsException extends HttpException {
   constructor(message?: string) {
